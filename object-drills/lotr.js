b1d91ebd7462cadb78ff911c59107001dc498c27
cfiander <cfiander90@gmail.com>
2019-05-01 14:44:40 -0700

    avengers

--- object-drills/lotr.js
+++ object-drills/lotr.js
@@ -16,14 +16,14 @@
 
 */
 
-const createCharacter = ({name, nickname, race, origin, attack, defense}) => ({
+const createCharacter = ({name, nickname, race, origin, attack, defense, weapon}) => ({
   name,
   nickname,
   race,
   origin,
   attack,
   defense,
-  describe(){ console.log(`${this.name} is a ${this.race} from ${this.origin}.`)},
+  describe(){ console.log(`${this.name} is a ${this.race} from ${this.origin} who uses a ${this.weapon}`)},
   evaluateFight (character) {
     let shownString = '';
     if (this.attack >= character.defense){
@@ -37,12 +37,36 @@ const createCharacter = ({name, nickname, race, origin, attack, defense}) => ({
       shownString += 'and you receive 0 damage.';
 
     } console.log(shownString);
-  }
+  },
+  weapon,
 });
 
 const Gandalf = createCharacter({name: 'Gandalf the White', nickname: 'gandalf', race: ' Wizard', origin: 'Middle Earth', attack: 10, defense: 6});
-const Bilbo = createCharacter({name: 'Bilbo Baggins ', nickname: 'bilbo', race: ' Hobbit', origin: 'The Shire', attack: 2, defense: 1});
+const Bilbo = createCharacter({name: 'Bilbo Baggins ', nickname: 'bilbo', race: 'Hobbit', origin: 'The Shire', attack: 2, defense: 1});
+const Frodo = createCharacter({name: 'Frodo Baggins', nickname: 'frodo', race: 'Hobbit', origin: 'The Shire', attack: 6, defense: 2 })
+const Aragorn = createCharacter({name: 'Aragorn son of Arathorn', nickname: 'aragorn', race: 'Man', origin: 'Dunnedain', attack: 6, defense: 8 })
+const Legolas = createCharacter({name: 'Legolass', nickname: 'legolas', race: 'Elf', origin: 'Woodland Realm', attack: 8, defense: 5 })
 
-console.log(Gandalf);
+let characters = [
+  Gandalf, Bilbo, Frodo, Aragorn, Legolas
+  ];
+
+/*console.log(Gandalf);
 
 Gandalf.evaluateFight(Bilbo);
+
+
+
+characters.push(createCharacter({name: 'Arwen Undomiel', nickname: 'arwen', race: 'Half-Elf of Rivendell', origin: 'Rivendell', attack: 7, defense: 7}));
+
+console.log(characters)
+
+console.log(characters.find(name => name.nickname === 'aragorn').describe());*/
+
+const Hobbits = characters.filter(character => character.race === 'Hobbit');
+console.log(Hobbits);
+const strong = characters.filter(character => character.attack > 5);
+console.log(strong);
+
+Gandalf.weapon = 'staff';
+Gandalf.describe();
